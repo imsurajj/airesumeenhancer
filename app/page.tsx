0d2aebc10d521fbac9f941603fc036c86043546a
Suraj <iamsurajbro946@gmail.com>
2024-10-22 20:30:36 +0530

    Finally First Launch #1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import HomePage from '@/components/HomePage'
+import Navbar from '@/components/Navbar'
 
 export default function Page() {
-  return <HomePage />
+  return (
+    <>
+      <Navbar />
+      <HomePage />
+    </>
+  )
 }
